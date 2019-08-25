17b046dacfa1188f9deee5d0b9d9b56ac65d8250
Kiryl Padhaiski <46563159+KirylPod@users.noreply.github.com>
2019-08-25 18:20:03 +0300

    Update and rename Basic Mathematical Operations to BasicMathematicalOperations.java

--- 8 kyu/BasicMathematicalOperations.java
+++ 8 kyu/BasicMathematicalOperations.java
@@ -1,7 +1,7 @@
-public class BasicOperations
+public class BasicMathematicalOperations
 {
-  public static Integer basicMath(String op, int v1, int v2)
-  {
+  public static Integer basicMath(String op, int v1, int v2){
+  
   int result = 0;
     if (op.equals("+")) {
     result = v1 + v2;
