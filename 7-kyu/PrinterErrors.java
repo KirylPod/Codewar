076e4f570933d6eccf26f9c0976164d9e3012336
Kiryl Padhaiski <46563159+KirylPod@users.noreply.github.com>
2019-08-25 18:17:48 +0300

    Update and rename Printer Errors to PrinterErrors.java

--- 7-kyu/PrinterErrors.java
+++ 7-kyu/PrinterErrors.java
@@ -1,4 +1,4 @@
-public class Printer {
+public class PrinterErrors {
     
     public static String printerError(String s) {
        return (s.length()-s.replaceAll("[n-z]","").length()) + "/" + s.length();
