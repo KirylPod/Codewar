bfb139f3ff9a5e207410c8e62b7eca0b6a8b2acb
Kiryl Padhaiski <46563159+KirylPod@users.noreply.github.com>
2019-08-25 18:14:05 +0300

    Update and rename Find the stray number to FindTheStrayNumber.java

--- 7-kyu/FindTheStrayNumber.java
+++ 7-kyu/FindTheStrayNumber.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-class Solution {
+class FindTheStrayNumber {
   static int stray(int[] numbers) {
     Arrays.sort(numbers);
     return (numbers[0] == numbers[1])? numbers[numbers.length-1] : numbers[0] ;  
