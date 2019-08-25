95be94f2e706635a1d64530cfccab297696a7d81
Kiryl Padhaiski <46563159+KirylPod@users.noreply.github.com>
2019-08-25 15:35:23 +0300

    Update and rename Find the unique number to FindTheUniqueNumber.java

--- 6 kyu/FindTheUniqueNumber.java
+++ 6 kyu/FindTheUniqueNumber.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
- public class Kata {
+ public class FindTheUniqueNumber {
     public static double findUniq(double arr[]) {
       Arrays.sort(arr);
       return arr[0] == arr[1] ? arr[arr.length - 1] : arr[0];
